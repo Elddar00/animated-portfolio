248a18740e319b2d99374a42945f73304f10f9e2
Elddar00 <eldaridrizovic00@gmail.com>
2024-05-31 17:20:17 +0200

    Hamburger Menu Button SVG Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import Test from "./Test";
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      <section>
+      <section id="Home">
         <Navbar />
       </section>
-      <section>Parallax</section>
+      <section id="Usluge">Parallax</section>
       <section>Services</section>
-      <section>Parallax</section>
+      <section id="Nasi Radovi">Parallax</section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
       <section>Portfolio4</section>
-      <section>Contact</section>
+      <section id="Kontakt">Contact</section>
       {/* <Test /> */}
     </div>
   );
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,5 +1,14 @@
 const Links = () => {
-  return <div className="links">Links</div>;
+  const items = ["Home", "Usluge", "Nasi Radovi", "Kontakt", "O nama"];
+  return (
+    <div className="links">
+      {items.map((item) => (
+        <a href={`#${item}`} key={item}>
+          {item}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default Links;
