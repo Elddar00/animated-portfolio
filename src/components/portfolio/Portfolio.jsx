89c4ea0244e024f616a87b9b42e77d7e2caed478
Elddar00 <eldaridrizovic00@gmail.com>
2024-06-05 03:15:52 +0200

    Framer Motion Animated Progress Bar

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -47,7 +47,7 @@ export const Portfolio = () => {
   return (
     <div className="portfolio" ref={ref}>
       <div className="progress">
-        <h1>Featured Works</h1>
+        <h1>Galerija osmeha :</h1>
         <motion.div style={{ scaleX }} className="progressBar"></motion.div>
       </div>
       {items.map((item) => (
