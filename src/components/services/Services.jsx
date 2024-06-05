864cbebd3a92ab37394078a50108942ff3d4baf3
Elddar00 <eldaridrizovic00@gmail.com>
2024-06-05 02:20:32 +0200

    Framer Motion Animate Services Section - implement useInView Hook

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,31 +1,62 @@
+import { useRef } from "react";
 import "./services.scss";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 export const Services = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
   return (
-    <motion.div className="services">
-      <motion.div className="textContainer">
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      //   animate="animate"
+      //   whileInView="animate"
+      ref={ref}
+      animate={isInView && "animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
         <p>
           Posvećen sam pomaganju pacijentima
           <br /> da ostvare zdrave i lepe osmehe.
         </p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
-          <img src="/osmeh1.CR2" alt="" />
+          {/* <img src="/osmeh1.CR2" alt="" /> */}
+          <img src="/osmeh01.jpg" alt="" />
           <h1>
-            <b>Postignite</b> Osmeh
+            <motion.b whileHover={{ color: "black" }}>Postignite</motion.b>{" "}
+            Osmeh
           </h1>
         </div>
         <div className="title">
           <h1>
-            <b>Svojih</b> Snova.
+            <motion.b whileHover={{ color: "black" }}>Svojih</motion.b> Snova.
           </h1>
           <button>Sta mi radimo?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
+      <motion.div className="listContainer" variants={variants}>
         <motion.div
           className="box"
           whileHover={{ background: "white", color: "black" }}
